045bfdaa2a147ec09c6fdf7fd3337e40bf0cbca7
Romanov Michael <Romanov.players@gmail.com>
2022-06-30 18:49:13 +0300

    реализовал слайдер секции товаров

--- js/team.js
+++ js/team.js
@@ -4,6 +4,9 @@ $(document).ready(function () {
         e.preventDefault();
         let showHide =$(e.currentTarget).siblings('.team__player-text').slideToggle(300);
         let toggleClass = $(e.currentTarget).toggleClass('team__player-link--active');
+        let another = $(e.currentTarget).closest('.team__list').find('.team__player-link');
+  
+        
    })
 
 });
