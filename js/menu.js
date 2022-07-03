3e06228400b8d4be2a0246242acf8714b46b470c
Romanov Michael <Romanov.players@gmail.com>
2022-07-03 15:50:54 +0300

    убрал скролл в выпадающем менюё

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,10 @@
 
 const hamburger = document.querySelector("#hamburger");
 
-
 hamburger.addEventListener("click", e => {
     e.preventDefault();
     const overlay = document.createElement("div");
+    document.body.classList.add("stop-scrolling");
     overlay.classList.add("overlay");
     overlay.innerHTML = document.querySelector("#overlay").innerHTML;
     document.body.append(overlay);
@@ -12,11 +12,15 @@ hamburger.addEventListener("click", e => {
     const close = document.querySelector(".close__overlay");
     close.addEventListener("click", e => {
         e.preventDefault();
-        document.body.removeChild(overlay);
+        document.body.classList.remove("stop-scrolling");
+      
     });
 
     overlay.addEventListener("click", e => {
         e.target == overlay
+        document.body.classList.remove("stop-scrolling");
         document.body.removeChild(overlay);
     })
 });
+
+
